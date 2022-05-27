50234c21a0b51fc45b360cdbb6e14c4cf1cbb0df
LENOVO <nrtechnologyid@gmail.com>
2022-05-27 15:54:28 +0700

    model

--- DataMhs/admin.py
+++ DataMhs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Mahasiswa
+
+
+class MahasiswaAdmin(admin.ModelAdmin):
+    list_display = ('Nim', 'Nama')
+
 
 # Register your models here.
+admin.site.register(Mahasiswa, MahasiswaAdmin)
--- DataMhs/models.py
+++ DataMhs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+KELAMIN_CHOICES = [
+    ('LK', 'Laki-Laki'),
+    ('PR', 'Perempuan'),
+]
+
+
 # Create your models here.
+class Mahasiswa(models.Model):
+    Nim = models.CharField(max_length=10)
+    Nama = models.CharField(max_length=255)
+    TmpLahir = models.CharField(max_length=255)
+    TglLahir = models.DateField()
+    Alamat = models.CharField(max_length=255)
+    Kelamin = models.CharField(max_length=2, choices=KELAMIN_CHOICES)
